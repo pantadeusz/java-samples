2a4c4d24a852604c3f2e04d743e2c372c0319fd3
Tadeusz <pantadeusz@pjwstk.edu.pl>
2017-05-15 20:32:19 +0200

    simplified jdbc example

--- jdbc/hellojdbc/src/test/java/pl/tau/dbunitdemo/service/PersonManagerTest.java
+++ jdbc/hellojdbc/src/test/java/pl/tau/dbunitdemo/service/PersonManagerTest.java
@@ -1,55 +1,32 @@
 package pl.tau.dbunitdemo.service;
-// przyklad na podstawie przykladow J. Neumanna
+
 import static org.junit.Assert.*;
-
-import java.sql.SQLException;
-import java.util.List;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 import pl.tau.dbunitdemo.domain.Person;
+import java.sql.*;
 
+import java.sql.SQLException;
+
+@RunWith(JUnit4.class)
 public class PersonManagerTest {
+    PersonManager personManager;
 
-	PersonManager personManager = new PersonManagerImpl();
-
-	public PersonManagerTest() throws SQLException {
-	}
-
-	@After
-    public void cleanup() throws SQLException {
-        //personManager.clearPersons();
+    public PersonManagerTest() throws SQLException {
+        String url = "jdbc:hsqldb:hsql://localhost/workdb";
+        personManager = new PersonManagerImpl(DriverManager.getConnection(url));
     }
 
-	@Before
-	public void filldata() throws SQLException {
-		//personManager.clearPersons();
-	}
+    @Test
+    public void checkAdding() throws Exception {
+        Person person = new Person();
+        person.setName("Janek");
+        person.setYob(1939);
 
-	@Test
-	public void checkConnection() {
-	    assertNotNull(personManager.getConnection());
-	}
-	
-	@Test
-	public void checkAdding() throws SQLException{
-		String NAME_1 = "Janek";
-		int YOB_1 = 1939;
-
-		Person person = new Person();
-		person.setName(NAME_1);
-		person.setYob(YOB_1);
-		
-		personManager.clearPersons();
-		assertEquals(1,personManager.addPerson(person));
-		
-		List<Person> persons = personManager.getAllPersons();
-		Person personRetrieved = persons.get(0);
-		
-		assertEquals(NAME_1, personRetrieved.getName());
-		assertEquals(YOB_1, personRetrieved.getYob());
-	}
+        assertEquals(1, personManager.addPerson(person));
+    }
 
 }
