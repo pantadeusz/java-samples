0a30b01a9ddde9a5ad9da60172e3a6eabc6deee8
pantadeusz <none@none.com>
2017-06-05 16:39:33 +0200

    made databases in memory

--- dbunit/hellodbunit/scripts/server.sh
+++ dbunit/hellodbunit/scripts/server.sh
@@ -5,7 +5,7 @@ cd $(dirname $0)
 cd ..
 
 #in memory database
-#java -cp lib/hsqldb.jar org.hsqldb.server.Server --database.0 mem:mydb --dbname.0 workdb
+java -cp lib/hsqldb.jar org.hsqldb.server.Server --database.0 mem:mydb --dbname.0 workdb
 
 #in file database
-java -cp lib/hsqldb.jar org.hsqldb.server.Server --database.0 file:/tmp/mydb --dbname.0 workdb
+#java -cp lib/hsqldb.jar org.hsqldb.server.Server --database.0 file:/tmp/mydb --dbname.0 workdb
